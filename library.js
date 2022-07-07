eb7898dee8b4af8149b4ddb759f70c7d9aa9022c
Starjun XBT <starjunxbt@gmx.cn>
2022-07-07 14:17:06 +0200

    Write a constructor for making “Book” objects.

---
+++ library.js
@@ -0,0 +1,11 @@
+function Book(title, author, pages, readed) {
+  return {
+    title,
+    author,
+    pages,
+    readed,
+  };
+}
+
+// const test = new Book("test", "Starjun", 50, "no");
+// console.log(test);
