701f608d47d3dfee31612875f35743ab2db22691
Starjun XBT <starjunxbt@gmx.cn>
2022-07-07 14:25:02 +0200

    Method to display info

--- library.js
+++ library.js
@@ -1,11 +1,16 @@
 function Book(title, author, pages, readed) {
-  return {
-    title,
-    author,
-    pages,
-    readed,
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.readed = readed;
+
+  this.info = function () {
+    const a = `${this.title} by ${this.author}, ${this.pages} pages`;
+    if (readed == "no") return a + ", not read yet";
+    return a + "readed";
   };
 }
 
 // const test = new Book("test", "Starjun", 50, "no");
 // console.log(test);
+// console.log(test.info());
